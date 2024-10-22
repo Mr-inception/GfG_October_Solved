146e3fdc73c2d6a5b3682f7f062c88d75e365295
ZealinShubh <shubhadeepsupriyo77330@gmail.com>
2024-10-22 15:05:38 +0530

    Added Solution for October 5

---
+++ GeeksforGeeks_Solutions/C++/October_05.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+class Solution{
+public:
+    long long findSmallest(vector<int> &arr) {
+        // Your code goes here.
+        long long ans = 1;
+        for(auto elm:arr){
+            if(ans>=elm){
+                ans += elm;
+            }
+        }
+        return ans;
+    }
+    };
